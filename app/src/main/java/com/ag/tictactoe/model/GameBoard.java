9e498bb36aafb97ce1767ac2e67ca96d45be373f
Anthony Giang <anthonyhgiang@gmail.com>
2020-03-26 17:10:06 -0700

    Refactored tile map name inside of GameBoard.

--- app/src/main/java/com/ag/tictactoe/model/GameBoard.java
+++ app/src/main/java/com/ag/tictactoe/model/GameBoard.java
@@ -30,7 +30,7 @@ public class GameBoard {
     /**
      * 2D array for the Tile board.
      */
-    private Tile[][] board;
+    private Tile[][] tileMap;
 
     /**
      * HashMap will map a Button id to a Tile.
@@ -41,13 +41,13 @@ public class GameBoard {
      * Default constructor initializes the 2D array of Tile.
      */
     public GameBoard() {
-        board = new Tile[NUMBER_OF_TILE_ROWS][NUMBER_OF_TILE_COLS];
+        tileMap = new Tile[NUMBER_OF_TILE_ROWS][NUMBER_OF_TILE_COLS];
         boardHashMap = new HashMap<Integer, Tile>();
 
         // Reference the new Tile object to the board.
         for (int i = 0; i < NUMBER_OF_TILE_ROWS; i++) {
             for (int j = 0; j < NUMBER_OF_TILE_COLS; j++) {
-                board[i][j] = new Tile();
+                tileMap[i][j] = new Tile();
             }
         }
     }
@@ -56,7 +56,7 @@ public class GameBoard {
      * Constructor initializes the 2D array of Tile and maps the Button to the Tile.
      */
     public GameBoard(List<View> buttons) {
-        board = new Tile[NUMBER_OF_TILE_ROWS][NUMBER_OF_TILE_COLS];
+        tileMap = new Tile[NUMBER_OF_TILE_ROWS][NUMBER_OF_TILE_COLS];
         boardHashMap = new HashMap<Integer, Tile>();
 
         int buttonCount = 0;
@@ -64,10 +64,10 @@ public class GameBoard {
         // Reference the new Tile object to the board.
         for (int i = 0; i < NUMBER_OF_TILE_ROWS; i++) {
             for (int j = 0; j < NUMBER_OF_TILE_COLS; j++) {
-                board[i][j] = new Tile();
+                tileMap[i][j] = new Tile();
                 // Map the Button id to the Tile.
                 if (buttonCount < buttons.size()) {
-                    boardHashMap.put(buttons.get(buttonCount).getId(), board[i][j]);
+                    boardHashMap.put(buttons.get(buttonCount).getId(), tileMap[i][j]);
                     buttonCount++;
                 } else {
                     Log.e(TAG, "Extra buttons detected in the game board.");
@@ -81,8 +81,8 @@ public class GameBoard {
      *
      * @return
      */
-    public Tile[][] getGameBoard() {
-        return board;
+    public Tile[][] getTileMap() {
+        return tileMap;
     }
 
     /**
